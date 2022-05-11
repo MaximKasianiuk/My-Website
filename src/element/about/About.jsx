c9443a9256f0f5547c458ba7fbe3468d6fa1ba09
MaximKasianiuk <maxim.99@hotmail.com>
2022-05-11 14:47:49 -0400

    updated

---
+++ src/element/about/About.jsx
@@ -0,0 +1,27 @@
+import './about.css'
+const About = () => {
+    return (
+        <div className='a'>
+            <div className='a-left'>
+                <div className='a-card bg'></div>
+                <div className='a-card'>
+                    <img src='../images/img-9.jpg' alt='' className='a-img'/>
+                </div>
+            </div>
+            <div className='a-right'>
+                <h2 className = 'a-title'>
+                    About Me
+                    </h2>
+                <p className ='a-sub'>
+                Dynamic recent Computer Science graduate open for new job opportunities. Passionate about the ever-evolving technology sector and continued personal development. Adept at working within strict deadlines and goals that are set by a manager but also able to work autonomously to complete projects. Willing to continue investing in the learning process to gain the necessary knowledge and practical experience to become an influence in the tech industry.
+
+                </p>
+                <p className='a-desc'>
+                    MORE STUFF MORE STUFF
+                </p>
+            </div>
+        </div>
+    )
+}
+export default About
+/*insert image here 26:47*/
